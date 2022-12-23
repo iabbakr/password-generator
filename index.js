4799e996d46b447786445aa5c43693fbbb74b9cc
iabbakr <abubakaribrahim603@gmail.com>
2022-12-23 11:49:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function passwordGenerator(){
         let pwdI2 = Math.floor(Math.random() * characters.length);
     
         pwd11.textContent = characters[pwdI1];
-        pwd22.textContent = characters[pwdI2];
+        pwd22.textContent = characters[pwdI2 * 15];
     };
 };
 
